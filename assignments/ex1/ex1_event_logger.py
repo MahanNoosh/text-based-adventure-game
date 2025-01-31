e69630f4b780c76af93338952632fdd0541fb241
Nizar <mohammed.alissa627@gmail.com>
2025-01-30 21:36:23 -0500

    Completed

--- assignments/ex1/ex1_event_logger.py
+++ assignments/ex1/ex1_event_logger.py
@@ -85,12 +85,10 @@ class EventList:
             print(f"Location: {curr.id_num}, Command: {curr.next_command}")
             curr = curr.next
 
-    # TODO: Complete the methods below, based on the given descriptions. Do NOT change any of their specification.
     #  That is, the function headers (parameters, return type, etc.) must NOT be changed.
     def is_empty(self) -> bool:
         """Return whether this event list is empty."""
 
-        # TODO: Your code below
         return self.first is None
 
     def add_event(self, event: Event, command: Optional[str] = None) -> None:
@@ -99,17 +97,14 @@ class EventList:
         event in the game.
         """
         # Hint: You should update the previous node's <next_command> as needed
-        # TODO: Your code below
         new = event
         new.prev = self.last
         if self.is_empty():
             self.first = new
         else:
             self.last.next = new
+            new.next_command = command
         self.last = new
-
-
-
 
     def remove_last_event(self) -> None:
         """Remove the last event from this event list.
@@ -117,23 +112,36 @@ class EventList:
 
         # Hint: The <next_command> and <next> attributes for the new last event should be updated as needed
 
-        # TODO: Your code below
+        if not self.is_empty():
+            if self.last == self.first:
+                self.last = None
+                self.first = None
+            else:
+                self.last = self.last.prev
+                self.last.next = None
+                self.last.next_command = None
 
     def get_id_log(self) -> list[int]:
         """Return a list of all location IDs visited for each event in this list, in sequence."""
 
-        # TODO: Your code below
+        id_list = []
+        curr = self.first
+        while curr:
+            id_list.append(curr.id_num)
+            curr = curr.next
+        return id_list
 
-    # Note: You may add other methods to this class as needed but DO NOT CHANGE THE SPECIFICATION OF ANY OF THE ABOVE
+
+# Note: You may add other methods to this class as needed but DO NOT CHANGE THE SPECIFICATION OF ANY OF THE ABOVE
 
 
 if __name__ == "__main__":
-    pass
+
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (Delete the "#" and space before each line.)
     # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
-    # import python_ta
-    # python_ta.check_all(config={
-    #     'max-line-length': 120,
-    #     'disable': ['R1705', 'E9998', 'E9999']
-    # })
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'disable': ['R1705', 'E9998', 'E9999']
+    })
